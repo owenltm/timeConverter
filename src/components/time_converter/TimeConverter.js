9e4cec2c8fd75e1dcf79d63ef4bbf533453d49fb
owenltm <owenlwiantoro01@gmail.com>
2022-01-24 15:53:47 +0800

    time convert finish

--- src/components/time_converter/TimeConverter.js
+++ src/components/time_converter/TimeConverter.js
@@ -1,16 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import dayjs from 'dayjs';
+
 import TimeInput from './TimeInput';
 
 function TimeConverter(props) {
+  const [times, setTimes] = useState([{
+    timezone: "Asia/Singapore",
+    time: 0
+  }, {
+    timezone: "PST8PDT",
+    time: 0
+  }])
 
-  const handleChange = (timezone, value) => {
-    console.log(timezone, value);
+  const handleChange = (index, timezone, value) => {
+
+    const hour = value / 1440 * 24;
+    const minute = value % 60;
+
+    const tempTime = dayjs().tz(timezone).hour(parseInt(hour)).minute(parseInt(minute));
+
+    setTimes(times.map((t, i) => {
+      if(i != index){
+
+        const newTime = tempTime.tz(t.timezone);
+
+        return {
+          ...t,
+          time: (newTime.hour() * 60) + newTime.minute()
+        }
+      } else {
+        return {
+          ...t,
+          time: parseInt(value)
+        };
+      }
+    }))
+
+    //console.log(times);
   }
 
   return (
     <div className='flex h-screen'>
-      <TimeInput handleChange={handleChange} />
-      <TimeInput handleChange={handleChange} />
+      {
+        times.map((time, index) => 
+          <TimeInput key={index} index={index} timeValue={time.time} timezoneValue={time.timezone} handleChange={handleChange} />
+        )
+      }
     </div>
   );
 }
--- src/components/time_converter/TimeInput.js
+++ src/components/time_converter/TimeInput.js
@@ -1,36 +1,43 @@
 import React, { useEffect, useState } from 'react';
-// import './TimeInput.css';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
 
-function TimeInput({timeValue, handleChange}) {
-  const [value, setValue] = useState(50);
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
+function TimeInput({index, timeValue, timezoneValue, handleChange}) {
   const [hourLbl, setHourLbl] = useState("12");
   const [minLbl, setMinLbl] = useState("00");
-  const [timezone, setTimezone] = useState("");
+
+  useEffect(() => {
+    //console.log(dayjs().tz(timezoneValue).format());
+  }, []);
 
   useEffect(() => {
     //convert slider to 24hrs value
-    const hour = value / 1000 * 24;
-    const minute = (value / 1000 * 1440) % 60;
+
+    const newTimeValue = timeValue % 1440;
+
+    const hour = newTimeValue / 1440 * 24;
+    const minute = newTimeValue % 60;
 
     setHourLbl(parseInt(hour) < 10 ? '0' + parseInt(hour) : parseInt(hour));
     setMinLbl(parseInt(minute) < 10 ? '0' + parseInt(minute) : parseInt(minute));
-
-    handleChange(timezone, value);
-    //console.log(hour, minute, value);
-  }, [value])
+  }, [timeValue]);
 
   const onZoneChange = (e) => {
-    setTimezone(e.target.value)
+    
   }
 
   const onTimeChange = (e) => {
-    setValue(e.target.value);
+    handleChange(index, timezoneValue, e.target.value);
   }
 
   return (
     <div className='flex-1 flex flex-col bg-blue-400'>
       <div className='flex justify-center py-4'>
-        <input className='p-2 rounded-md text-center' type='text' value={timezone} onChange={onZoneChange} />
+        <input className='p-2 rounded-md text-center' type='text' value={timezoneValue} onChange={onZoneChange} />
       </div>
       <div className='flex-1 flex flex-col justify-evenly items-center text-white'>
         {/* <div className='bg-yellow-200 w-1/3 h-20'>
@@ -38,10 +45,10 @@ function TimeInput({timeValue, handleChange}) {
         </div> */}
         <div className='w-3/4'>
           <div className='text-center mb-5'>
-            <h1 className='text-3xl'>Mon, 24 Jan 2022</h1>
+            {/* <h1 className='text-3xl'>Mon, 24 Jan 2022</h1> */}
             <h2 className='text-5xl font-bold'>{hourLbl}:{minLbl}</h2>
           </div>
-          <input className='text-red-100' type='range' min={0} max={1000} value={value} onChange={onTimeChange}/>
+          <input className='text-red-100' type='range' min={0} max={1439} value={timeValue % 1440} onChange={onTimeChange}/>
         </div>
       </div>
     </div>
