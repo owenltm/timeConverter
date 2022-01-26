0302226f36a13f8bf3726ae87351898ec801cc7a
owenltm <owenlwiantoro01@gmail.com>
2022-01-26 17:03:12 +0800

    switch to ms timestamp value

--- src/components/time_converter/TimeInput.js
+++ src/components/time_converter/TimeInput.js
@@ -6,25 +6,22 @@ import timezone from 'dayjs/plugin/timezone';
 dayjs.extend(utc)
 dayjs.extend(timezone)
 
-function TimeInput({index, timeValue, timezoneValue, handleChange}) {
-  const [hourLbl, setHourLbl] = useState("12");
-  const [minLbl, setMinLbl] = useState("00");
-
+function TimeInput({index, timeValue, zoneValue, handleChange}) {
   useEffect(() => {
-    //console.log(dayjs().tz(timezoneValue).format());
+    //console.log(dayjs().tz(zoneValue).format());
     //test
   }, []);
 
   useEffect(() => {
     //convert slider to 24hrs value
 
-    const newTimeValue = timeValue % 1440;
+    /* const newTimeValue = timeValue % 1440;
 
     const hour = newTimeValue / 1440 * 24;
     const minute = newTimeValue % 60;
 
     setHourLbl(parseInt(hour) < 10 ? '0' + parseInt(hour) : parseInt(hour));
-    setMinLbl(parseInt(minute) < 10 ? '0' + parseInt(minute) : parseInt(minute));
+    setMinLbl(parseInt(minute) < 10 ? '0' + parseInt(minute) : parseInt(minute)); */
   }, [timeValue]);
 
   const onZoneChange = (e) => {
@@ -32,13 +29,26 @@ function TimeInput({index, timeValue, timezoneValue, handleChange}) {
   }
 
   const onTimeChange = (e) => {
-    handleChange(index, timezoneValue, e.target.value);
+    const sliderValue = e.target.value
+    
+    handleChange(index, zoneValue, sliderToTimestamp(sliderValue));
+  }
+
+  const timestampToSlider = (timestamp) => {
+    return (timestamp.hour() * 60) + timestamp.minute()
+  }
+
+  const sliderToTimestamp = (slider) => {
+    const hour = slider / 1440 * 24;
+    const minute = slider % 60;
+
+    return dayjs().tz(zoneValue).hour(parseInt(hour)).minute(parseInt(minute));
   }
 
   return (
     <div className='flex-1 flex flex-col bg-blue-400'>
       <div className='flex justify-center py-4'>
-        <input className='p-2 rounded-md text-center' type='text' value={timezoneValue} onChange={onZoneChange} />
+        <input className='p-2 rounded-md text-center' type='text' value={zoneValue} onChange={onZoneChange} />
       </div>
       <div className='flex-1 flex flex-col justify-evenly items-center text-white'>
         {/* <div className='bg-yellow-200 w-1/3 h-20'>
@@ -46,10 +56,10 @@ function TimeInput({index, timeValue, timezoneValue, handleChange}) {
         </div> */}
         <div className='w-3/4'>
           <div className='text-center mb-5'>
-            {/* <h1 className='text-3xl'>Mon, 24 Jan 2022</h1> */}
-            <h2 className='text-5xl font-bold'>{hourLbl}:{minLbl}</h2>
+            <h1 className='text-3xl'>{timeValue.format('ddd, D MMM YY')}</h1>
+            <h2 className='text-5xl font-bold'>{timeValue.format('HH:mm')}</h2>
           </div>
-          <input className='text-red-100' type='range' min={0} max={1439} value={timeValue % 1440} onChange={onTimeChange}/>
+          <input className='text-red-100' type='range' min={0} max={1439} value={timestampToSlider(timeValue)} onChange={onTimeChange}/>
         </div>
       </div>
     </div>
