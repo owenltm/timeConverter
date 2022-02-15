a96504d0c3a35d32efbc04f5ae8e6d532f7a0d32
owenltm <owenlwiantoro01@gmail.com>
2022-02-15 17:55:22 +0800

    add timeinput button

--- src/components/time_converter/TimeConverter.js
+++ src/components/time_converter/TimeConverter.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { PlusIcon } from '@heroicons/react/solid';
 import dayjs from 'dayjs';
 import advancedFormat from 'dayjs/plugin/advancedFormat';
 
@@ -7,6 +8,7 @@ import TimeInput from './TimeInput';
 dayjs.extend(advancedFormat)
 
 function TimeConverter(props) {
+  //base times
   const [times, setTimes] = useState([{
     timezone: "Asia/Singapore",
     time: dayjs().tz("Asia/Singapore")
@@ -14,6 +16,20 @@ function TimeConverter(props) {
     timezone: "PST8PDT",
     time: dayjs().tz("PST8PDT")
   }])
+
+  const handleAddTime = (position) => {
+    if(position === 0) {
+      setTimes([{
+        timezone: "PST8PDT",
+        time: dayjs().tz("PST8PDT")
+      }, ...times])
+    } else {
+      setTimes([...times, {
+        timezone: "PST8PDT",
+        time: dayjs().tz("PST8PDT")
+      }])
+    }
+  }
 
   const handleChange = (index, timezone, value) => {
     const tempTime = dayjs(value).tz(timezone);
@@ -36,11 +52,21 @@ function TimeConverter(props) {
 
   return (
     <div className='flex h-screen'>
+      <div className='side-btn-container px-2 bg-sky-400 flex h-screen'>
+        <button onClick={() => handleAddTime(0)}>
+          <PlusIcon className="h-12 w-12 text-white"/>
+        </button>
+      </div>
       {
         times.map((time, index) => 
           <TimeInput key={index} index={index} timeValue={time.time} zoneValue={time.timezone} handleChange={handleChange} />
         )
       }
+      <div className='side-btn-container px-2 bg-sky-400 flex h-screen'>
+        <button onClick={() => handleAddTime(1)}>
+          <PlusIcon className="h-12 w-12 text-white"/>
+        </button>
+      </div>
     </div>
   );
 }
